863bddfce4fa292db0fc05b0c39dadc5cc7bc642
Jiang Wu <john.x.wu@ericsson.com>
2021-02-04 16:29:51 +0200

    use env to set redis cluster network setting

--- config/conf.js
+++ config/conf.js
@@ -1,3 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+const configFile = path.resolve(process.env.REDIS_CLUSTER_NETWORK_JSON || __dirname + '/redis_cluster_network.json')
+
+let redisClusterNetworkConf;
+
+if (process.env.DEV_ENVIRONMENT !== 'dev') {
+    const data = fs.readFileSync(configFile, 'UTF-8').toString()
+    redisClusterNetworkConf = JSON.parse(data)
+    console.log("Read config file:" + configFile.toString() + " ->")
+    console.log(redisClusterNetworkConf)
+}
+
 const conf = {
     mysql: {
         host: process.env.DATABASE_HOST || "127.0.0.1",
@@ -6,20 +19,7 @@ const conf = {
         database: process.env.DATABASE_DB_NAME || "testdb",
         port: process.env.DATABASE_PORT || 3306,
     },
-    redisCluster: [
-        {
-            host: process.env.REDIS_NODE1_HOST,
-            port: process.env.REDIS_NODE1_PORT || 6379,
-        },
-        {
-            host: process.env.REDIS_NODE2_HOST,
-            port: process.env.REDIS_NODE2_PORT || 6379,
-        },
-        {
-            host: process.env.REDIS_NODE3_HOST,
-            port: process.env.REDIS_NODE3_PORT || 6379,
-        }
-    ],
+    redisCluster: redisClusterNetworkConf,
     redisSingleton: {
         host: process.env.REDIS_HOST || "localhost",
         port: process.env.REDIS_PORT || 6379,
